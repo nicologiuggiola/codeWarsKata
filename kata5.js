fd0d80d439d8e2d61f04a751831276ee3e1ec312
nicologiuggiola <93972864+nicologiuggiola@users.noreply.github.com>
2022-02-16 06:37:51 +0100

    last push

--- kata5.js
+++ kata5.js
@@ -12,4 +12,10 @@ function divisors(n) {
     return array.length === 0 ? n + " is prime" : array;
 }
 
-console.log(divisors(997));
+//console.log(divisors(997));
+
+function getSlope(p1, p2) {
+    return p1[0] === p2[0] ? null : (p2[1] - p1[1])/(p2[0] - p1[0])
+}
+
+//Game Hit the target - 2nd part
