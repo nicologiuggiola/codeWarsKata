f7bd5e79c07b3060b39b0effeb4bb24237009508
vivianabobbio <93972864+nicologiuggiola@users.noreply.github.com>
2022-02-16 11:15:49 +0100

    push lezione 16/02

--- kata5.js
+++ kata5.js
@@ -19,3 +19,74 @@ function getSlope(p1, p2) {
 }
 
 //Game Hit the target - 2nd part
+
+function getMiddle(string) {
+    return string.length % 2 !== 0 ? string[Math.floor(string.length/2)] : string[(string.length/2)-1] + string[(string.length/2)];
+}
+
+function getMiddle2(string) {
+    return [...string].reduce((p,c,i,a) => a.length % 2 === 0 ? string.substring((i/2),(i/2)+2) : a[Math.floor(i/2)]);
+}
+
+function getMiddle3(string) {
+    return string.slice((string.length-1)/2, string.length/2+1);
+}
+
+console.log(getMiddle3("citao"));
+
+function solution(n) {
+    let array = []
+
+    for (let i = 1; i < n; i++) {
+        if (i%3 === 0 || i%5=== 0) {
+            array.push(i);
+        }
+    }
+    return array.length === 0 ? 0 : array.reduce((p,c) => p += c);
+}
+
+function solution2(number) {
+    return number < 0 ? 0 : [...new Array(number).keys()].reduce((p,c) => (c%3 === 0 || c%5=== 0) ? p + c : p, 0);
+}
+
+console.log(solution(2));
+
+function incrementString(string) {
+    let lastChar = string[string.length-1];
+    if (isNaN(lastChar)) {
+        return string +1
+    }
+    let importantIndex;
+    for (let i = string.length -2; i >= 0; i--){
+        if (isNaN(string[i])) {
+            importantIndex = i;
+            break;
+        }
+    }
+    const partialString = string.substring(0, importantIndex + 1);
+    const stringNumber = parseInt(string.substring(importantIndex + 1));
+
+    return partialString + (stringNumber + 1);
+}
+
+function incrementString2(string) {
+    const regex = RegExp(/\d+$/);
+    let searchResult = regex.exec(string);
+    if (!searchResult) {
+        return string + 1;
+    } else {
+        let numberFoundString = searchResult[0];
+        const numberInt = parseInt(numberFoundString);
+        return string.substring(0,string.length - numberFoundString.length) + (numberInt + 1);
+    }
+}
+
+function incrementString3(string) {
+    return !(/\d+$/).exec(string) ? string + 1 : string.substring(0,string.length - (/\d+$/).exec(string)[0].length) + (parseInt((/\d+$/).exec(string)) + 1);
+}
+
+console.log(incrementString3("montagna"));
+
+// function findMissing(array){
+//     return array.reduce((p,c) => c , 0)
+// }
